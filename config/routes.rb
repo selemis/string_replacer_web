3ec808bb6f73efb5a03acbcc815c020e64a7b348
Stelios Omirou <selemis@gmail.com>
2015-06-07 14:29:22 +0300

    more styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+
+
   resources :replacements
 
   resources :values
 
   resources :templates
 
-  get 'replace/:id' => 'replacements#replace'
-  get 'download/:id' => 'replacements#download'
+  get 'replace/:id' => 'replacements#replace', as: :replace
+  get 'download/:id' => 'replacements#download', as: :download
+
+  root 'replacements#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
