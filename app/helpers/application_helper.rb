3d44af27654a93b8680e8a5459dceeffdd71454a
Stelios Omirou <selemis@gmail.com>
2015-06-07 19:07:13 +0300

    active nav link and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def nav_link(link_text, link_path, css_class)
+    class_name = current_page?(link_path) ? "#{css_class}" : ''
+    content_tag(:li, class: class_name) do
+      link_to(content_tag(:span, link_text), link_path)
+    end
+  end
+
+
 end
